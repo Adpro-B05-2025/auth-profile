d9b24d674354978e7890c1e8d5d8a992c58ab60c
farel39 <argyafarel.kasyara@gmail.com>
2025-05-02 17:43:39 +0700

    [REFACTOR] Refactor ProfileController with new strategy.

---
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/config/AspectConfig.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.authprofile.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+    // Configuration for aspect-oriented programming
+}
