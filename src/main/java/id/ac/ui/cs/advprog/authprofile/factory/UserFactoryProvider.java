2b6980c7e133c780b39038494f6e6186dd55f760
farel39 <argyafarel.kasyara@gmail.com>
2025-05-02 13:52:26 +0700

    [REFACTOR] Refactor UserFactoryProvider class to adhere to the OCP principle

--- src/main/java/id/ac/ui/cs/advprog/authprofile/factory/UserFactoryProvider.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/factory/UserFactoryProvider.java
@@ -1,38 +1,55 @@
 package id.ac.ui.cs.advprog.authprofile.factory;
 
 import id.ac.ui.cs.advprog.authprofile.dto.request.BaseRegisterRequest;
-import id.ac.ui.cs.advprog.authprofile.dto.request.RegisterCareGiverRequest;
-import id.ac.ui.cs.advprog.authprofile.dto.request.RegisterPacillianRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Provider class that returns the appropriate factory based on the request type
+ * Provider class that returns the appropriate factory based on the request type.
+ * This implementation follows the Open/Closed Principle by using a registry of factories.
  */
 @Component
 public class UserFactoryProvider {
 
-    private final PacillianFactory pacillianFactory;
-    private final CareGiverFactory careGiverFactory;
+    private final Map<Class<? extends BaseRegisterRequest>, UserFactory> factoryRegistry = new HashMap<>();
 
+    /**
+     * Constructs a new UserFactoryProvider with a list of factories.
+     * Each factory registers itself for the request types it can handle.
+     *
+     * @param factories list of all UserFactory implementations
+     */
     @Autowired
-    public UserFactoryProvider(PacillianFactory pacillianFactory, CareGiverFactory careGiverFactory) {
-        this.pacillianFactory = pacillianFactory;
-        this.careGiverFactory = careGiverFactory;
+    public UserFactoryProvider(List<UserFactory> factories) {
+        // Each factory registers the request types it can handle
+        for (UserFactory factory : factories) {
+            for (Class<? extends BaseRegisterRequest> requestType : factory.getSupportedRequestTypes()) {
+                factoryRegistry.put(requestType, factory);
+            }
+        }
     }
 
     /**
      * Returns the appropriate user factory based on the request type
      * @param registerRequest the registration request
      * @return the user factory
+     * @throws IllegalArgumentException if no factory is registered for the request type
      */
     public UserFactory getFactory(BaseRegisterRequest registerRequest) {
-        if (registerRequest instanceof RegisterPacillianRequest) {
-            return pacillianFactory;
-        } else if (registerRequest instanceof RegisterCareGiverRequest) {
-            return careGiverFactory;
-        } else {
-            throw new IllegalArgumentException("Unsupported registration request type");
+        if (registerRequest == null) {
+            throw new IllegalArgumentException("Register request cannot be null");
         }
+
+        UserFactory factory = factoryRegistry.get(registerRequest.getClass());
+        if (factory == null) {
+            throw new IllegalArgumentException("Unsupported registration request type: " +
+                    registerRequest.getClass().getName());
+        }
+
+        return factory;
     }
 }
