6bc7842490ba8826b03420299acf85212eee3d26
farel39 <argyafarel.kasyara@gmail.com>
2025-05-23 20:32:48 +0700

    chore: add async configuration and enable in app

--- src/main/java/id/ac/ui/cs/advprog/authprofile/AuthProfileApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/AuthProfileApplication.java
@@ -2,9 +2,11 @@ package id.ac.ui.cs.advprog.authprofile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
+@EnableAsync
 public class AuthProfileApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/config/AsyncConfig.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.authprofile.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "asyncTaskExecutor")
+    public Executor asyncTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+}
