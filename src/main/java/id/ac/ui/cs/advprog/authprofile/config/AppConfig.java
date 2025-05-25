61027bd84d1d66baeed558deb8f6647d038728c1
farel39 <argyafarel.kasyara@gmail.com>
2025-05-25 17:17:22 +0700

    Update AppConfig

--- src/main/java/id/ac/ui/cs/advprog/authprofile/config/AppConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/authprofile/config/AppConfig.java
@@ -1,14 +1,33 @@
 package id.ac.ui.cs.advprog.authprofile.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 
+    @Value("${rating.service.connection.timeout:5000}")
+    private int connectionTimeout;
+
+    @Value("${rating.service.read.timeout:10000}")
+    private int readTimeout;
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(clientHttpRequestFactory());
+        return restTemplate;
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(connectionTimeout);
+        factory.setReadTimeout(readTimeout);
+        return factory;
     }
 }
